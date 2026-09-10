4549387dd1c01363cab0a14c4c751d773456bcd8
othello7 <othello71@outlook.com>
2026-09-09 21:34:03 -0400

    remove 2fps limit on crop detecting

--- bot/cropdetect.sh
+++ bot/cropdetect.sh
@@ -1,1 +1,1 @@
-ffmpeg -i $1 -vf fps=2,cropdetect=16:2:0 -f null - 2>&1|awk '/crop/{print $NF}'|tail -n1| cut -d ':' -f 3,4
+ffmpeg -i $1 -vf cropdetect=16:2:0 -f null - 2>&1|awk '/crop/{print $NF}'|tail -n1| cut -d ':' -f 3,4
